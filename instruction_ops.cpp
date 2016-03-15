fdb054ea5838581d6dbf18c13f096e5798b5b40e
Zhenduo Liang <liangzhenduo0608@hotmail.com>
2016-03-15 21:46:41 +0800

    init

---
+++ instruction_ops.cpp
@@ -0,0 +1,7 @@
+#include "ppc_altivec.h"
+
+//TODO: add more instruction here 
+
+void vadduhm(ppc_avr_t &r, ppc_avr_t &a, ppc_avr_t &b){                                                               
+    //TODO: body of instruction                                                           
+}
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include "ppc_altivec.h"
+
+using namespace std;
+
+int main(){
+    ppc_avr_t avr[32];
+
+    ifstream infile1("vector_gpr.txt");
+
+	//read input file
+    while(!infile1.eof()){
+        for(int i=0; i<32; i++){
+            infile1.ignore(3);
+            infile1>>hex>>avr[i].u32[0]>>avr[i].u32[1]>>avr[i].u32[2]>>avr[i].u32[3];
+        }
+    }
+
+    string ins;
+    int vra,vrb,vrt;
+
+	//read instruction
+    ifstream infile2("instruction.txt");
+
+    while(!infile2.eof()){
+        infile2>>ins>>vrt>>vra>>vrb;
+        if(ins == "vadduhm")
+                vadduhm(avr[vrt], avr[vra], avr[vrb]);
+		//TODO:add more instruction
+    }
+	
+	//output the result
+    ofstream outfile("vector_gpr.txt");
+    for(int i=0; i<32; i++)
+        	outfile<<setw(2)<<dec<<setfill('0')<<i<<" "
+                 <<setw(8)<<hex<<setfill('0')<<avr[i].u32[0]<<" "
+                 <<setw(8)<<hex<<setfill('0')<<avr[i].u32[1]<<" "
+                 <<setw(8)<<hex<<setfill('0')<<avr[i].u32[2]<<" "
+                 <<setw(8)<<hex<<setfill('0')<<avr[i].u32[3]<<endl;
+}
