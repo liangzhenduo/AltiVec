7a58b0567f7271f84f7e928c73378e58cfbca3de
Zhenduo Liang <liangzhenduo0608@hotmail.com>
2016-04-14 17:53:42 +0800

    fixed avg

--- verilog/vavgsh.v
+++ verilog/vavgsh.v
@@ -8,8 +8,8 @@ wire   [31 : 0] vrt;
 wire   [15 : 0] op0;
 wire   [15 : 0] op1;
 
-assign op0	= 	((vra[ 7: 0] + vrb[ 7: 0] + 1) >> 1) & 16'hffff;
-assign op1	= 	((vra[15: 8] + vrb[15: 8] + 1) >> 1) & 16'hffff;
+assign op0	= 	((vra[15: 0] + vrb[15: 0] + 1) >> 1) & 16'hffff;
+assign op1	= 	((vra[31:16] + vrb[31:16] + 1) >> 1) & 16'hffff;
 assign vrt	=	{op1, op0};            
 
 endmodule
